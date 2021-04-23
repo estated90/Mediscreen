dfeb679ec23ad59de1748ec1b2c9c64a16730a6b
estated90 <50859250+estated90@users.noreply.github.com>
2021-04-23 10:04:04 +0200

    Test working and repeatable. MongoDb for test is cleaned and dropped

--- historic/src/test/java/web/historic/service/HistoricServiceTest.java
+++ historic/src/test/java/web/historic/service/HistoricServiceTest.java
@@ -4,24 +4,28 @@ import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
+import static org.springframework.data.mongodb.core.FindAndModifyOptions.options;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.time.LocalDate;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
@@ -32,8 +36,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.dozermapper.core.inject.Inject;
 
 import web.historic.dto.HistoricDto;
+import web.historic.model.DatabaseSequence;
+import web.historic.model.Historic;
 import web.historic.model.Patient;
 import web.historic.proxy.PatientFeign;
+import web.historic.repositories.HistoricRepositories;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -44,20 +51,30 @@ class HistoricServiceTest {
 
 	@Autowired
 	private MockMvc mockMvc;
+	@Autowired
+	private HistoricRepositories repository;
 	@MockBean
 	@Inject
 	private PatientFeign patientFeign;
 	@Inject
 	private HistoricService hitoricService;
+	@Autowired
+	private MongoOperations mongoOperations;
+
+	@AfterEach
+	void tearDownAfterClass() throws Exception {
+		repository.deleteAll();
+		mongoOperations.dropCollection("database_sequences");
+	}
 
 	@Test
 	@Order(1)
 	void whenRunningApp_thenCorrectFunctionningImpl() throws Exception {
-		//GIVEN
+		// GIVEN
 		Patient patient = new Patient(1, "Test", "Test", LocalDate.now(), "F", "Test", "Test");
 		// WHEN
 		when(patientFeign.getPatientById(anyInt())).thenReturn(patient);
-		//THEN
+		// THEN
 		mockMvc.perform(MockMvcRequestBuilders.get("/historic/1"))
 				.andExpect(MockMvcResultMatchers.status().isNotFound())
 				.andExpect(jsonPath("$.message", is("No history has been found for patient 1")))
@@ -67,7 +84,11 @@ class HistoricServiceTest {
 		historic.setPatient("Test");
 		historic.setPractitionnerNotesRecommandation("Some note");
 		mockMvc.perform(put("/historic/create").contentType(MediaType.APPLICATION_JSON).content(asJsonString(historic)))
-				.andExpect(status().isCreated());
+				.andExpect(status().isCreated()).andExpect(jsonPath("$.patId", is(historic.getPatId())))
+				.andExpect(jsonPath("$.patient", is(historic.getPatient())))
+				.andExpect(jsonPath("$.practitionnerNotesRecommandation",
+						is(historic.getPractitionnerNotesRecommandation())))
+				.andExpect(jsonPath("$.id", is(1)));
 	}
 
 	public static String asJsonString(final Object obj) {
