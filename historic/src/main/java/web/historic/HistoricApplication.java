87d3208f293f1b0e970ee65eedcfd03f3d4d16dd
Nico <nicolas.detesta90@gmail.com>
2021-04-28 11:44:14 +0200

    Add swagger project
    Add second db test in docker my sql
    Add swagger

--- historic/src/main/java/web/historic/HistoricApplication.java
+++ historic/src/main/java/web/historic/HistoricApplication.java
@@ -5,8 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @ComponentScan("web.historic")
+@EnableSwagger2
 @EnableFeignClients
 public class HistoricApplication {
 
